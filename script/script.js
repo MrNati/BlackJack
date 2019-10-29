4c20fad446c0e07fb5ca1569d33c42bcbc058485
Emeka Ananti <emekaananti@gmail.com>
2019-10-29 22:39:55 +0100

    Create card1 and card2 variables

---
+++ script/script.js
@@ -0,0 +1,11 @@
+//card variables
+let card1 = 'Ace of Spades',
+    card2 = 'Ten of Hearts';
+
+//welcome message
+console.log('Welcome to Blackjack!');
+
+//card in play
+console.log('You are dealt: ');
+console.log(' ' + card1);
+console.log(' ' + card2);
