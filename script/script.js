50e7fe120771bcea5be55229b2351e877b13b8a0
Emeka Ananti <emekaananti@gmail.com>
2019-11-03 20:28:14 +0100

    Add all 52 cards to deck

--- script/script.js
+++ script/script.js
@@ -1,5 +1,18 @@
+//available cards
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+
+let values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 
+'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two'];
+
 //card in deck
-let deck = ["Ace of Spades", "Two of Spades", "Three of Spades"];
+let deck = [];
+
+//add available cards to the deck
+for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+    for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+        deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+    }
+}
 
 //player card
 let playerCards = [deck[0], deck[2]];
